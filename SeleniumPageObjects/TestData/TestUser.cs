9db749382fc4c98ae273bb6e80b8001846f61f3b
rajeev.k24 <rajeev.k24@outlook.com>
2020-06-30 23:43:35 +0100

    Removed code

--- SeleniumPageObjects/TestData/TestUser.cs
+++ SeleniumPageObjects/TestData/TestUser.cs
@@ -5,6 +5,6 @@
         public string UserName { get; set; }
         public string PassWord { get; set; }
 
-        public Gender GenderType { get; set; }
+       // public Gender GenderType { get; set; }
     }
 }
